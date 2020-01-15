2ea1aa32c80dc1d34a5092f34ec0e7afc0f5ad30
EC2 Default User <ec2-user@ip-172-31-31-38.ec2.internal>
2020-01-15 01:08:38 +0000

    Daily update of issue data

--- cron-job/git.sh
+++ cron-job/git.sh
@@ -4,4 +4,4 @@ cd /home/ec2-user/find-an-issue
 git pull
 git add .
 git commit -m "Daily update of issue data"
-git push --repo https://jmuzsik:Tohav8lov8@github.com/find-an-issue.git
+git push --repo https://jmuzsik:Tohav8lov8@github.com/jmuzsik/find-an-issue.git
